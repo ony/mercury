13d639d78ef63c124900f505ee1b8ff1404c849f
Bert Thompson <aet@cs.mu.oz.au>
1997-09-29 09:00:52 +0000

    makestubs is a utility for generating C stub files corresponding to bytecode files. Such stub files are used to allow compiled code to call bytecode.
    Currently, this is nowhere near complete.

---
+++ bytecode/makestub.h
@@ -0,0 +1,15 @@
+
+/*
+** Copyright (C) 1997 University of Melbourne.
+** This file may only be copied under the terms of the GNU Library General
+** Public License - see the file COPYING.LIB in the Mercury distribution.
+**
+** $Id: makestub.h,v 1.1.2.1 1997-09-29 09:00:52 aet Exp $
+*/
+
+
+#ifndef MB_TEMPLATE_H
+#define	MB_TEMPLATE_H
+
+
+#endif	/* MB_TEMPLATE_H */
