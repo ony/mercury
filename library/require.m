ce2750aedda6d82433fdcfc7f102e7278e3774e4
David Overton <dmo@cs.mu.oz.au>
2001-02-06 02:18:23 +0000

    Checkin on the mode-constraints branch the work I've done so far on the new
    Estimated hours taken: 1000
    
    Checkin on the mode-constraints branch the work I've done so far on the new
    constraint-based mode checker.
    
    ----------------------------------------------------------------------
    Modified Files:
     Tag: mode-constraints
    	Mmake.common.in Mmakefile
    	compiler/dependency_graph.m compiler/goal_path.m
    	compiler/hlds_goal.m compiler/hlds_module.m
    	compiler/hlds_pred.m compiler/maybe_mlds_to_gcc.pp
    	compiler/mercury_compile.m compiler/modules.m
    	compiler/options.m compiler/rl_file.pp compiler/rl_out.pp
    	library/Mmakefile library/eqvclass.m library/library.m
    	library/require.m library/term.m library/varset.m
    	robdd/Makefile robdd/bryant.c robdd/bryant.h runtime/mercury.h
    	runtime/mercury_heap.h runtime/mercury_init.h
    	runtime/mercury_memory.h runtime/mercury_reg_workarounds.c
    	runtime/mercury_reg_workarounds.h
    Added Files:
     Tag: mode-constraints
    	.mode-constraints-sync-tag compiler/hhf.m
    	compiler/inst_graph.m compiler/mode_constraint_robdd.m
    	compiler/mode_constraints.m compiler/mode_ordering.m
    	library/robdd.m robdd/Mmakefile tools/modes_sync
    ----------------------------------------------------------------------

--- library/require.m
+++ library/require.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1993-1999 The University of Melbourne.
+% Copyright (C) 1993-1999, 2001 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -18,6 +18,8 @@
 
 :- pred error(string).
 :- mode error(in) is erroneous.
+
+:- func abort(string) = _ is erroneous.
 
 %	error(Message).
 %		Throw a `software_error(Message)' exception.
@@ -88,6 +90,9 @@ report_lookup_error(Msg, K, V) :-
 
 error(Message) :- 
 	throw(software_error(Message)).
+
+abort(Message) = _ :-
+	error(Message).
 
 :- end_module require.
 
