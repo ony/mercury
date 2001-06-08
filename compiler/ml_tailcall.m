9eed5b8a957257f7172d767d6972f3e6fde8b270
Peter Ross <petdr@cs.mu.oz.au>
2001-06-08 09:46:46 +0000

    Merge dotnet-foreign branch upto trunk_snapshot_20010608192944.
    Estimated hours taken: 0.25
    Branches: dotnet-foreign
    
    Merge dotnet-foreign branch upto trunk_snapshot_20010608192944.

--- compiler/ml_tailcall.m
+++ compiler/ml_tailcall.m
@@ -456,7 +456,8 @@ check_const(Const, Locals) :-
 	; Const = data_addr_const(DataAddr) ->
 		DataAddr = data_addr(ModuleName, DataName),
 		( DataName = var(VarName) ->
-			\+ var_is_local(qual(ModuleName, VarName), Locals)
+			\+ var_is_local(
+				qual(ModuleName, ModuleName, VarName), Locals)
 		;
 			true
 		)
@@ -481,7 +482,7 @@ check_const(Const, Locals) :-
 var_is_local(Var, Locals) :-
 		% XXX we ignore the ModuleName --
 		% that is safe, but overly conservative
-	Var = qual(_ModuleName, VarName),
+	Var = qual(_PackageName, _ModuleName, VarName),
 	some [Local] (
 		locals_member(Local, Locals),
 		Local = data(var(VarName))
@@ -505,7 +506,7 @@ function_is_local(CodeAddr, Locals) :-
 	),
 		% XXX we ignore the ModuleName --
 		% that is safe, but might be overly conservative
-	QualifiedProcLabel = qual(_ModuleName, ProcLabel),
+	QualifiedProcLabel = qual(_PackageName, _ModuleName, ProcLabel),
 	ProcLabel = PredLabel - ProcId,
 	some [Local] (
 		locals_member(Local, Locals),
