f8e4f951c744d0b851b1d830a369c27a7778d30a
Julien Fischer <juliensf@csse.unimelb.edu.au>
2006-10-11 07:51:43 +0000

    Add a TODO list at the top of this module.
    Estimated hours taken: 1
    Branches: stream_branch
    
    library/stream.m:
    	Add a TODO list at the top of this module.
    
    	Provide more documentation.
    
    	Provide generic versions of the input_stream folds from the
    	the io module.
    
    	Delete the comment about calling `seekable' `random_access'.
    	I prefer `seekable' now.

--- library/stream.m
+++ library/stream.m
@@ -9,11 +9,19 @@
 % File: stream.m.
 % Authors: juliensf, maclarty.
 %
+% TODO:
+%   - non-blocking streams.
+%   - thread-safety (really an issue for the instances).
+%   - add more generic operations.
+%   - where do the flush and fill operations belong?
+%   - what about resizable buffers?
+%
 %-----------------------------------------------------------------------------%
 
 :- module stream.
 :- interface.
 
+:- import_module bool.
 :- import_module string.
 
 %-----------------------------------------------------------------------------%
@@ -41,13 +49,17 @@
 % Streams
 %
 
-    % A stream consists of a base, a state to update and an error type.
+    % A stream consists of a handle type, a unique state type and an
+    % error type.  The handle allows us to refer to different instances
+    % of a stream type.  The state type is threaded through the stream
+    % operations, while the error type encapsulates errors that might
+    % be produced by the underlying stream instances.
     %
 :- typeclass stream.stream(Stream, State, Error)
     <= (stream.error(Error), (Stream -> State, Error)) where
 [ 
-        % A human readable name describing the stream, for use in 
-        % error messages etc.
+        % Returns a descriptive name for the stream.
+        % Intended for use in error messages.
         %
         pred name(Stream::in, stream.name::out, State::di, State::uo) is det
 ].
@@ -57,6 +69,9 @@
 % Input streams
 %
 
+    % An input stream is a stream from which we can read things(?) of
+    % type Unit.
+    %
 :- typeclass stream.input(Stream, Unit, State, Error)
     <= stream(Stream, State, Error) where
 [
@@ -69,6 +84,9 @@
 % Output streams
 %
 
+    % An output stream is a stream to which we can write things(?) of
+    % type Unit.
+    %
 :- typeclass stream.output(Stream, Unit, State, Error)
     <= stream(Stream, State, Error) where
 [
@@ -80,6 +98,8 @@
 % Duplex streams
 %
 
+    % A duplex stream is one to which we can both read and write.
+    %
 :- typeclass stream.duplex(Stream, Unit, State, Error)
     <= ( stream.input(Stream,  Unit, State, Error),
          stream.output(Stream, Unit, State, Error)
@@ -134,8 +154,6 @@
     ;       cur
     ;       end.
 
-    % XXX call this random_access?
-    %
 :- typeclass stream.seekable(Stream, State, Error)
          <= stream(Stream, State, Error) where [
      pred seek(Stream::in, stream.whence::in, int::in, State::di, State::uo)
@@ -170,8 +188,12 @@
 
 %-----------------------------------------------------------------------------%
 %
-% Generic stream operations
+% Generic folds over input streams
 %
+
+:- type stream.res(Error)
+    --->    ok
+    ;       error(Error).
 
 :- type stream.maybe_partial_res(T, Error)
     --->    ok(T)
@@ -187,6 +209,44 @@
     in, out, di, uo) is det.
 :- mode stream.input_stream_fold(in, in(pred(in, in, out) is cc_multi),
     in, out, di, uo) is cc_multi.
+
+    % Applies the given closure to each Unit read from the input stream
+    % in turn, until eof or error.
+    %
+:- pred stream.input_stream_fold_state(Stream, pred(Unit, State, State),
+    stream.res(Error), State, State)
+    <= stream.input(Stream, Unit, State, Error).
+:- mode stream.input_stream_fold_state(in, in(pred(in, di, uo) is det),
+    out, di, uo) is det.
+:- mode stream.input_stream_fold_state(in, in(pred(in, di, uo) is cc_multi),
+    out, di, uo) is cc_multi.
+    
+    % Applies the given closure to each Unit read from the inpu stream
+    % in turn, until eof or error.
+    %
+:- pred stream.input_stream_fold2_state(Stream,
+    pred(Unit, T, T, State, State), T, stream.maybe_partial_res(T, Error),
+    State, State) <= stream.input(Stream, Unit, State, Error).
+:- mode stream.input_stream_fold2_state(in,
+    in(pred(in, in, out, di, uo) is det),
+    in, out, di, uo) is det.
+:- mode stream.input_stream_fold2_state(in,
+    in(pred(in, in, out, di, uo) is cc_multi),
+    in, out, di, uo) is cc_multi.
+
+    % Applies the given closure to each Unit read from the input stream
+    % in turn, until eof or error, or the closure returns `no' as its
+    % second argument.
+    %
+:- pred stream.input_stream_fold2_state_maybe_stop(Stream,
+    pred(Unit, bool, T, T, State, State),
+    T, stream.maybe_partial_res(T, Error), State, State)
+    <= stream.input(Stream, Unit, State, Error).
+:- mode stream.input_stream_fold2_state_maybe_stop(in,
+    in(pred(in, out, in, out, di, uo) is det), in, out, di, uo) is det.
+:- mode stream.input_stream_fold2_state_maybe_stop(in,
+    in(pred(in, out, in, out, di, uo) is cc_multi), in, out, di, uo)
+    is cc_multi.
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -212,6 +272,54 @@ stream.input_stream_fold(Stream, Pred, T0, Res, !S) :-
         Res = error(T0, Error)
     ).
 
+stream.input_stream_fold_state(Stream, Pred, Res, !S) :-
+    get(Stream, Result0, !S),
+    (
+        Result0 = ok(Result),
+        Pred(Result, !S),
+        stream.input_stream_fold_state(Stream, Pred, Res, !S)
+    ;
+        Result0 = eof,
+        Res = ok
+    ;
+        Result0 = error(Error),
+        Res = error(Error)
+    ).
+
+stream.input_stream_fold2_state(Stream, Pred, T0, Res, !S) :-
+    get(Stream, Result0, !S),
+    (
+        Result0 = ok(Result),
+        Pred(Result, T0, T1, !S),
+        stream.input_stream_fold2_state(Stream, Pred, T1, Res, !S)
+    ;
+        Result0 = eof,
+        Res = ok(T0)
+    ;
+        Result0 = error(Error),
+        Res = error(T0, Error)
+    ).
+
+stream.input_stream_fold2_state_maybe_stop(Stream, Pred, T0, Res, !S) :-
+    get(Stream, Result0, !S),
+    (
+        Result0 = ok(Result),
+        Pred(Result, Continue, T0, T1, !S),
+        (
+            Continue = no,
+            Res = ok(T1)
+        ;
+            Continue = yes,
+            stream.input_stream_fold2_state_maybe_stop(Stream, Pred, T1, Res,
+                !S)
+        )
+    ;
+        Result0 = eof,
+        Res = ok(T0)
+    ;
+        Result0 = error(Error),
+        Res = error(T0, Error)
+    ).
 
 %-----------------------------------------------------------------------------%
 :- end_module stream.
